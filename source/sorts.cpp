e1bf262c854cd97da5ba62cc7d2d1f3e40bae86f
camrenwoodstock <camrenwoodstock@gmail.com>
2019-06-06 16:33:14 -0400

    MergeSort Done

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -25,31 +25,31 @@ void swap(int * y, int * x) {
   *x = temp;
 }
 
-void selection_sort(int* arr, int n) {
+// void selection_sort(int* arr, int n) {
  
-  for(int i = 0; i < (n-1); i++) {
-    int min = i;
-    for (int j = i + 1; j < n ; j++) {
-        if(arr[j] < arr[min]) min = j;
-    }
-    swap(&arr[min],&arr[i]);
+//   for(int i = 0; i < (n-1); i++) {
+//     int min = i;
+//     for (int j = i + 1; j < n ; j++) {
+//         if(arr[j] < arr[min]) min = j;
+//     }
+//     swap(&arr[min],&arr[i]);
     
-  }
-}
+//   }
+// }
 
-void insertion_sort(int* arr, int n) {
-    for(int i = 0; i < n; i++) {
-      // Set j = i 
-      // j > 0 -> for each j from i to 0
-      // if the element at j-1 is greater than the element at j 
-      // -> arr[j] < arr[j-1]
-      for(int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
-        //then swap the elements
-        swap(&arr[j],&arr[j-1]);
-      }
-    }
+// void insertion_sort(int* arr, int n) {
+//     for(int i = 0; i < n; i++) {
+//       // Set j = i 
+//       // j > 0 -> for each j from i to 0
+//       // if the element at j-1 is greater than the element at j 
+//       // -> arr[j] < arr[j-1]
+//       for(int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
+//         //then swap the elements
+//         swap(&arr[j],&arr[j-1]);
+//       }
+//     }
 
-}
+// }
 
 
 
@@ -107,3 +107,45 @@ void insertion_sort(int* arr, int n)
   
  }
  
+// mergesort
+
+void merge(int* arr, int n, int mid) {
+  // Declare new array and copy elements
+  int temp[n];
+  for(int i = 0; i < n; i++) {
+    temp[i] = arr[i];
+  }
+  int j = 0;
+  int k = mid;
+  for (int i = 0; i < n; i++) {
+    if (j >= mid) {
+      arr[i] = temp[k];
+      k++;
+    }
+    else if (k >= n) {
+      arr[i] = temp[j];
+      j++;
+    }
+    else if (temp[j] < temp[k]) {
+      arr[i] = temp[j];
+      j++;
+    }
+    else {
+      arr[i] = temp[k];
+      k++;
+    }
+  }
+
+
+}
+
+void mergesort(int* arr, int n) {
+  int start = 0;
+  int end = n-1;
+  int mid = n / 2;
+    if(start < end) {
+        mergesort(arr, mid);
+        mergesort(arr + mid, n);
+        merge(arr, n, end);
+    }
+}
